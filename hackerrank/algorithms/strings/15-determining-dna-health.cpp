cab359c1e7c98f6aa6487006f3b66a6414dc7649
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2023-12-26 17:14:18 -0500

    Knutt-Morris-Pratt, not fast enough

--- hackerrank/algorithms/strings/15-determining-dna-health.cpp
+++ hackerrank/algorithms/strings/15-determining-dna-health.cpp
@@ -1,11 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
 struct DNA {
     int start;
     int end;
     string d;
 };
 
+void populateLPS(vector<int> &lps, const string &pattern) {
+    int j = 0;
+    lps[0] = 0;
+    for (int i = 1; i < pattern.length();) {
+        if (pattern[i] == pattern[j]) {
+            lps[i] = ++j;
+            ++i;
+        } else {
+            if (j > 0) {
+                j = lps[j - 1];
+            } else {
+                lps[i] = 0;
+                ++i;
+            }
+        }
+    }
+}
+
+int searchKMP(const string &pattern, const string &text) {
+    int numOccurrences = 0;
+    int j = 0;
+    vector<int> lps(pattern.length());
+    populateLPS(lps, pattern);
+    for (int i = 0; text.length() - i >= pattern.length() - j;) {
+        if (pattern[j] == text[i]) {
+            ++j;
+            ++i;
+        }
+        if (j == pattern.length()) {
+            // Found pattern
+            ++numOccurrences;
+            j = lps[j - 1];
+        } else if (i < text.length() && pattern[j] != text[i]) {
+            if (j > 0) {
+                j = lps[j - 1];
+            } else {
+                ++i;
+            }
+        }
+    }
+    return numOccurrences;
+}
+
 void determineHealth(vector<string> genes, vector<int> health, vector<DNA> dnaStrands) {
-    
+    int minHealth = INT_MAX, maxHealth = 0;
+    for (int i = 0; i < dnaStrands.size(); ++i) {
+        int currentHealth = 0;
+        for (int j = dnaStrands[i].start; j <= dnaStrands[i].end; ++j) {
+            currentHealth += searchKMP(genes[j], dnaStrands[i].d) * health[j];
+        }
+        if (currentHealth > maxHealth) {
+            maxHealth = currentHealth;
+        }
+        if (currentHealth < minHealth) {
+            minHealth = currentHealth;
+        }
+    }   
+    cout << minHealth << " " << maxHealth << endl;
 }
 
 int main()
@@ -67,4 +131,43 @@ int main()
     determineHealth(genes, health, dnaStrands);
 
     return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
 }
